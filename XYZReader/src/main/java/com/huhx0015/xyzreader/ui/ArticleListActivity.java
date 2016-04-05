189d07cad4a3cf179ea2f730295e4755d0e62fe0
huhx0015 <mhuh@yoonhuh.com>
2016-04-04 19:16:02 -0700

    feat: Replaced deprecated ActionBarActivity with AppCompatActivity.
    - Replaced deprecated ActionBarActivity with AppCompatActivity.

--- XYZReader/src/main/java/com/huhx0015/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/huhx0015/xyzreader/ui/ArticleListActivity.java
@@ -10,6 +10,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -29,7 +30,7 @@ import com.huhx0015.xyzreader.data.UpdaterService;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public class ArticleListActivity extends ActionBarActivity implements
+public class ArticleListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
     private Toolbar mToolbar;
