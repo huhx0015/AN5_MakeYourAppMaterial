56b2469ee6c3a5b553af1ef2750122a6e75a2ce8
huhx0015 <mhuh@yoonhuh.com>
2016-04-18 20:46:08 -0700

    activity_article_list.xml: Fixed dimensions with the Toolbar and the RecyclerView.

--- XYZReader/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ XYZReader/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -14,12 +14,12 @@ public final class R {
 		public static final int stackFromEnd = 0x7f0100e1;
 	}
 	public static final class dimen {
-		public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080075;
-		public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f080076;
-		public static final int item_touch_helper_swipe_escape_velocity = 0x7f080077;
+		public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080079;
+		public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f08007a;
+		public static final int item_touch_helper_swipe_escape_velocity = 0x7f08007b;
 	}
 	public static final class id {
-		public static final int item_touch_helper_previous_elevation = 0x7f0a0005;
+		public static final int item_touch_helper_previous_elevation = 0x7f0d0005;
 	}
 	public static final class styleable {
 		public static final int[] RecyclerView = { 0x010100c4, 0x7f0100de, 0x7f0100df, 0x7f0100e0, 0x7f0100e1 };
